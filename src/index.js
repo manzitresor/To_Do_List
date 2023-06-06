11704997065bbd7bc13861a2956d9967b6c56caa
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-06-06 17:26:10 +0200

    Add HTML Mark up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+
+const tasks = [
+  {
+    descr: 'wash the dishes',
+    completed: true,
+    index: 1,
+  },
+];
+
+const taskContainer = document.querySelector('.task-container');
+
+tasks.forEach((task) => {
+  let listTask = document.createElement('ul');
+  listTask = `
+    <li>${task.descr}</li>
+    <li>${task.completed}</li>
+    `;
+  taskContainer.innerHTML += listTask;
+});
