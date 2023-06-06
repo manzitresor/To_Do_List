963e83a8b2cb6822c2748d7323f93d643ac66b38
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-06-06 20:14:35 +0200

    Add created styles for to do task

--- src/index.js
+++ src/index.js
@@ -6,15 +6,28 @@ const tasks = [
     completed: true,
     index: 1,
   },
+  {
+    descr: 'complete To Do list Project',
+    completed: false,
+    index: 1,
+  },
 ];
 
 const taskContainer = document.querySelector('.task-container');
 
 tasks.forEach((task) => {
-  let listTask = document.createElement('ul');
-  listTask = `
-    <li>${task.descr}</li>
-    <li>${task.completed}</li>
-    `;
-  taskContainer.innerHTML += listTask;
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+            <div class= "container">
+            <div class='task-content'>
+            <input type="checkbox" id="task-${task.index}" ${task.completed ? 'checked' : ''}>
+            <label for="task-${task.index}" class="descr">${task.descr}</label>
+            </div>
+            <div class="icon-container">
+            <i class="fas fa-ellipsis-v"></i>
+            </div>
+            </div>
+            <hr>
+  `;
+  taskContainer.appendChild(listItem);
 });
