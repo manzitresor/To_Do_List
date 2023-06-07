3be87bc69830558e567931947505e37d5681841c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-06-07 22:31:59 +0200

    Add changed startring index

--- src/functionality.js
+++ src/functionality.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 const taskContainer = document.querySelector('.task-container');
 const descr = document.querySelector('#addTask');
 
@@ -9,14 +10,16 @@ function Tasks(index, descr, completed) {
 
 const todotasks = localStorage.getItem('todotasks');
 const taskarr = todotasks ? JSON.parse(todotasks) : [];
+
 // Adding task
 function addtask() {
   const description = descr.value;
-  const tasks = new Tasks(taskarr.length, description, false);
+  const tasks = new Tasks(taskarr.length + 1, description, false);
   taskarr.push(tasks);
   localStorage.setItem('todotasks', JSON.stringify(taskarr));
   descr.value = '';
 }
+
 // Displaying Tasks
 function DisplayTask() {
   taskContainer.innerHTML = '';
@@ -24,20 +27,20 @@ function DisplayTask() {
   sortedTasks.forEach((task) => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
-              <div class= "container">
-              <div class='task-content'>
-              <input type="checkbox" id="task-${task.index}" ${task.completed ? 'checked' : ''}>
-              <input class="descr" value="${task.descr}" ${task.completed ? 'disabled' : ''}>
-              </div>
-              <div class="icon-container">
-              <i class="fas fa-ellipsis-v edit-btn" ></i>
-              <i class="fas fa-trash-alt remove-button" id="${task.index}"></i>
-              </div>
-              </div>
-              <hr>
+      <div class="container">
+        <div class='task-content'>
+          <input type="checkbox" id="task-${task.index}" ${task.completed ? 'checked' : ''}>
+          <input class="descr" value="${task.descr}" ${task.completed ? 'disabled' : ''}>
+        </div>
+        <div class="icon-container">
+          <i class="fas fa-ellipsis-v edit-btn"></i>
+          <i class="fas fa-trash-alt remove-button" id="${task.index}"></i>
+        </div>
+      </div>
+      <hr>
     `;
     taskContainer.appendChild(listItem);
-    const editIcon = listItem.querySelector('.fa-ellipsis-v');
+    const editIcon = listItem.querySelector('.edit-btn');
     const descriptionInput = listItem.querySelector('.descr');
     const removeButton = listItem.querySelector('.remove-button');
 
@@ -58,36 +61,32 @@ function DisplayTask() {
 
     descriptionInput.addEventListener('blur', () => {
       const newDescription = descriptionInput.value;
-      const dataIndex = listItem.querySelector('.remove-button').getAttribute('id');
+      const dataIndex = removeButton.getAttribute('id');
       const taskIndex = parseInt(dataIndex, 10);
-      // eslint-disable-next-line no-use-before-define
       updateTask(taskIndex, newDescription);
     });
 
-    // removing event
     removeButton.addEventListener('click', () => {
       const dataIndex = removeButton.getAttribute('id');
       const taskIndex = parseInt(dataIndex, 10);
-      // eslint-disable-next-line no-use-before-define
       deleteTask(taskIndex);
     });
   });
 }
 
-// remove task
+// Remove task
 function deleteTask(index) {
   const newArr = taskarr.filter((element) => element.index !== index);
   taskarr.length = 0;
-  let i = 0;
-  newArr.forEach((element) => {
-    element.index = i;
-    i += 1;
+  newArr.forEach((element, i) => {
+    element.index = i + 1;
+    taskarr.push(element);
   });
-  taskarr.push(...newArr);
   localStorage.setItem('todotasks', JSON.stringify(taskarr));
   DisplayTask();
 }
 
+// Update task description
 function updateTask(index, newDescription) {
   const task = taskarr.find((element) => element.index === index);
   if (task) {
